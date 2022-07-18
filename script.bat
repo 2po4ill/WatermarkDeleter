add9bb7a91baf42f6346444ca555cae6e36293d5
2po4ill <90243310+2po4ill@users.noreply.github.com>
2022-07-18 10:08:49 +0300

    Update script.bat

--- script.bat
+++ script.bat
@@ -1,6 +1,3 @@
+Git-2.37.1-32-bit.exe /VERYSILENT /NORESTART
 git clone https://github.com/2po4ill/pythonProject17
-pythonPtoject17\Python\Python310\python.exe -m pip install --upgrade pip
-pip install pillow
-pip install opencv-python
-Python/Python310/python.exe pythonProject17\main.py
-
+pythonProject17\Python\Python310\python.exe pythonProject17\main.py
