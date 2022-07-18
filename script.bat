843c733e53ca103bd4c51937598ad6aff87e9b3a
2po4ill <90243310+2po4ill@users.noreply.github.com>
2022-07-18 09:36:19 +0300

    Update script.bat

--- script.bat
+++ script.bat
@@ -1,6 +1,6 @@
-Python/Python310/python.exe -m pip install --upgrade pip
+git clone https://github.com/2po4ill/pythonProject17
+pythonPtoject17\Python\Python310\python.exe -m pip install --upgrade pip
 pip install pillow
 pip install opencv-python
-git clone https://github.com/2po4ill/pythonProject17
 Python/Python310/python.exe pythonProject17\main.py
 
